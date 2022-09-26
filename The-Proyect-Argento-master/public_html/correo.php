55d659c81eec269268cb6d505636a58bee3dda02
patricioherrera2022 <herrerapatricio281@gmail.com>
2022-09-25 21:20:57 -0300

    actualizar

---
+++ The-Proyect-Argento-master/public_html/correo.php
@@ -0,0 +1,19 @@
+<?php
+
+
+if ( ! isset($_POST['name'])){
+  header ("location: index.html");
+}
+$name = $_POST['name']
+var_dump($nombre);
+
+$_POST['Email']
+$_POST['subject']
+$_POST['message']
+$_POST['']
+
+
+
+
+?>
+
